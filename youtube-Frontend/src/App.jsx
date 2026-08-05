fe799dc17e218a75274b6bac55ef91336335a192
Hasnain Laghari <lagharimuneeb0@gmail.com>
2026-08-05 18:10:29 +0500

    navbar created

--- youtube-Frontend/src/App.jsx
+++ youtube-Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import Layout from "./components/layout/Layout.jsx"
 
 const App = () => {
   return (
-    <div className='w-screen h-screen dark:bg-[#0F0F0F] dark:text-[#FFFFFF] bg-[#FFFFFF] text-black '>
+    <div className='w-full min-h-screen dark:bg-[#0F0F0F] dark:text-[#FFFFFF] bg-[#FFFFFF] text-black '>
       <Layout/>
     </div>
   )
--- youtube-Frontend/src/components/layout/Navbar.jsx
+++ youtube-Frontend/src/components/layout/Navbar.jsx
@@ -1,27 +1,41 @@
 import React from 'react'
-import { Menu, Search } from 'lucide-react'
+import { Menu, Search, Mic, Plus , Bell } from 'lucide-react'
 import youtubeHome from "../../assets/social.png"
 
 const Navbar = () => {
   return (
-    <div className='w-full border flex items-center  py-3 px-4'>
-      <div className="leftPart flex">
+    <div className='Navbar w-full flex items-center justify-between px-4 py-1  relative top-2'>
+      <div className="leftPart flex items-center space-x-6">
         <Menu />
-          <div className='w-[45px] h-[45px]'>
-            <img className='w-full h-full' src={youtubeHome}/>
-          </div>
-      </div>
-
-      <div className="searchPart">
-        <div className="search">
-          <input type="text" id='searchBar' />
-          <Search />
+        <div className='w-11 h-11'>
+          <img className='w-full h-full' src={youtubeHome} />
         </div>
       </div>
 
-      <div className="rightPart">
-          <p>Right part</p>
+      <div className="search flex items-center dark:bg-[#121212] dark:text-white text-black bg-white max-w-146 h-10 w-full dark:border dark:border-[#484646d0] rounded-[20px] mx-10">
+        <input className='relative z-2 placeholder:text[#6C6C6C] text-[17px] focus:outline-blue-500 focus:outline-1 outline-0 flex-1 w-full h-full rounded-tl-[20px] rounded-bl-[20px] pl-4' type="text" id='searchBar' placeholder='Search' />
+        <button className='searchBtn cursor-pointer bg-[#222222]  h-full flex items-center px-6 rounded-br-[20px] rounded-tr-[20px]'>
+          <Search />
+        </button>
       </div>
+
+      <div className="rightPart flex items-center  space-x-10 ">
+
+        <button className='p-3 rounded-[50%]'> {/* mic button*/}
+          <Mic size={24} />
+        </button>
+
+        <button className='flex items-center py-3 px-5 space-x-2 rounded-[20px]'> {/*create button*/}
+          <Plus size={25} /> 
+          <span className='text-[16px]'>Create</span>
+        </button>
+
+        <button className='p-3 rounded-[20px]'> {/* notifications button*/}
+          <Bell size={24} />
+        </button>
+
+      </div>
+
     </div>
   )
 }
